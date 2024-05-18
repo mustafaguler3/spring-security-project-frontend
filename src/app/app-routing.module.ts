17cb9660db1829591dd5b459903bddd51f049016
mustafaguler3 <mustafa.glr@hotmail.com>
2024-05-18 13:37:07 +0300

    add needed components and its routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { HomeComponent } from './home/home.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostResolverService } from './service/post-resolver.service';
+import { ProfileComponent } from './profile/profile.component';
+import { authenticationGuard } from './guard/authentication.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login",component: LoginComponent},
+  {path:"register",component: RegisterComponent},
+  {path:"resetPassword",component: ResetPasswordComponent},
+  {path:"",redirectTo:"/home",pathMatch: "full"},
+  {path:"home",component: HomeComponent,canActivate: [authenticationGuard]},
+  {path:"post/:postId",component: PostDetailComponent,resolve: {resolvedPost: PostResolverService},canActivate:[authenticationGuard]},
+  {path:"profile/:username",component: ProfileComponent,canActivate: [authenticationGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetPasswordComponent,
+    HomeComponent,
+    ProfileComponent,
+    NavbarComponent,
+    PostDetailComponent
   ],
   imports: [
     BrowserModule,

c81c1a2958f2844e716bc5f3a964fb5c31e2f889
mustafaguler3 <mustafa.glr@hotmail.com>
2024-05-18 13:28:40 +0300

    add spinner service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Alert } from './model/alert';
+import { SpinnerService } from './service/spinner.service';
+import { AlertService } from './service/alert.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'user-portal';
+export class AppComponent implements OnInit,OnDestroy{
+  private subscriptions: Subscription[] = []
+  alerts: Alert[] = []
+  loading!: boolean;
+
+  constructor (private spinnerService: SpinnerService,
+               private alertService: AlertService){
+          this.loading = false;
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.spinnerService.isLoading.subscribe(isLoading => {
+        this.loading = isLoading
+      })
+    );
+    this.subscriptions.push(
+      this.alertService.alerts.subscribe(alert => {
+        this.alerts.push(alert)
+        this.closeAlert(3)
+      })
+    )
+  }
+
+  private closeAlert(second: number):void {
+    setTimeout(() => {
+      const element: HTMLElement = document.getElementById("dismissAlert") as HTMLElement;
+      element.click();
+    },second * 1000)
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+  
 }
